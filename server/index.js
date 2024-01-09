9664a938213428aa999e99b09e2737967a011e53
flowergirlthatcodes3 <149510282+flowergirlthatcodes3@users.noreply.github.com>
2024-01-09 16:43:49 -0500

    ..

--- server/index.js
+++ server/index.js
@@ -1,29 +1,28 @@
-const {
-  seed,
-  client,
-} = require('./db');
-const express = require('express');
+const { seed, client } = require("./db");
+const express = require("express");
 const app = express();
 app.use(express.json());
-const path = require('path');
+const path = require("path");
 
-app.get('/', (req, res)=> res.sendFile(path.join(__dirname, '../public/index.html')));
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "../public/index.html"))
+);
 
-app.use('/dist', express.static(path.join(__dirname, '../dist')));
-app.use('/public', express.static(path.join(__dirname, '../public')));
+app.use("/dist", express.static(path.join(__dirname, "../dist")));
+app.use("/public", express.static(path.join(__dirname, "../public")));
 
-app.use('/api', require('./api'));
+app.use("/api", require("./api"));
 
-const init = async()=> {
+const init = async () => {
   await client.connect();
-  console.log('connected to database');
+  console.log("connected to database");
   await seed();
-  console.log('create your tables and seed data');
+  console.log("create your tables and seed data");
 
   const port = process.env.PORT || 3000;
-  app.listen(port, ()=> {
+  app.listen(port, () => {
     console.log(`listening on port ${port}`);
   });
-}
+};
 
 init();
